894f804df76578907f590a5bcf0eec55908b3b23
Loper <7byloper@gmail.com>
2026-05-16 16:33:31 +0300

    release 1.0.3

--- core/src/main/java/ru/loper/suncore/api/config/RedisConfig.java
+++ core/src/main/java/ru/loper/suncore/api/config/RedisConfig.java
@@ -9,13 +9,15 @@ public class RedisConfig {
     private final int port;
     private final String password;
     private final long timeout;
+    private final boolean enable;
 
     public static RedisConfig fromSection(ConfigurationSection section) {
         final String host = section.getString("host", "127.0.0.1");
         final int port = section.getInt("port", 6379);
         final String password = section.getString("password", "");
         final long timeout = section.getLong("timeout", 3000);
+        final boolean enable = section.getBoolean("enable", true);
 
-        return new RedisConfig(host, port, password, timeout);
+        return new RedisConfig(host, port, password, timeout, enable);
     }
 }
--- core/src/main/java/ru/loper/suncore/api/redis/RedisManager.java
+++ core/src/main/java/ru/loper/suncore/api/redis/RedisManager.java
@@ -18,7 +18,22 @@ public class RedisManager {
     private final StatefulRedisConnection<String, String> connection;
     private final StatefulRedisPubSubConnection<String, String> pubSubConnection;
 
+    private final RedisMessageListener listener;
+    private final boolean enabled;
+
     public RedisManager(RedisConfig config, RedisMessageListener listener, String... channels) {
+        this.listener = listener;
+        this.enabled = config.isEnable();
+
+        instances.add(this);
+
+        if (!enabled) {
+            redisClient = null;
+            connection = null;
+            pubSubConnection = null;
+            return;
+        }
+
         RedisURI redisUri = RedisURI.Builder.redis(config.getHost(), config.getPort())
                 .withPassword(config.getPassword().toCharArray())
                 .build();
@@ -29,7 +44,6 @@ public class RedisManager {
 
         pubSubConnection.addListener(listener);
         pubSubConnection.async().subscribe(channels);
-        instances.add(this);
     }
 
     public void onDisable() {
@@ -41,10 +55,15 @@ public class RedisManager {
     }
 
     public void sendCommand(String channel, String message) {
+        if (!enabled) {
+            listener.message(channel, message);
+            return;
+        }
+
         connection.sync().publish(channel, message);
     }
 
     public static void shutdownAll() {
-        instances.forEach(RedisManager::onDisable);
+        Set.copyOf(instances).forEach(RedisManager::onDisable);
     }
 }
