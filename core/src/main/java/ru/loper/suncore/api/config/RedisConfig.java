17e08f50e9b5a23b280970df32f28a61a9c10a2c
Loper <7byloper@gmail.com>
2026-05-18 16:42:59 +0300

    release 1.0.3

--- core/src/main/java/ru/loper/suncore/api/config/RedisConfig.java
+++ core/src/main/java/ru/loper/suncore/api/config/RedisConfig.java
@@ -12,6 +12,10 @@ public class RedisConfig {
     private final boolean enable;
 
     public static RedisConfig fromSection(ConfigurationSection section) {
+        if (section == null) {
+            return new RedisConfig("127.0.0.1", 6379, "", 300, false);
+        }
+
         final String host = section.getString("host", "127.0.0.1");
         final int port = section.getInt("port", 6379);
         final String password = section.getString("password", "");
